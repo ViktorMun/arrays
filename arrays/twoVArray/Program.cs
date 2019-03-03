00111f2892b84aaffc7d6f8018db0cf7f101b59e
Viktor <v.mun@yahoo.com>
2019-03-03 16:59:41 +0100

    Task5(a)

---
+++ arrays/twoVArray/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using twoArrayLib;
+
+
+
+namespace twoVArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Arrayoftwo arr = new Arrayoftwo(2, 0,6);
+          
+            Console.WriteLine(arr.ToString());
+            Console.WriteLine(arr.Sum);
+            Console.WriteLine(arr.SumMax);
+            Console.WriteLine(arr.Max);
+            Console.WriteLine(arr.Min);
+            Console.ReadKey();
+        }
+    }
+
+}
